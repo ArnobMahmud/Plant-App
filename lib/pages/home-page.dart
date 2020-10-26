e5b6c2df7bf5a1b002827b60b5cea2af83565ad5
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-26 11:13:21 +0600

    home page ui colored

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -5,9 +5,39 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff32a05f),
-      body: Column(children: [
-        
-      ],),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(100.0)),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                        height: 50, width: 50, child: Icon(Icons.arrow_back)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 140,
+          )
+        ],
+      ),
     );
   }
 }
