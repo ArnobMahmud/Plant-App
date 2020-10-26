7d628bfdb86f9cdfd5932f966eb746efa272249b
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-26 11:39:38 +0600

    data added

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -19,23 +19,71 @@ class HomePage extends StatelessWidget {
                 color: Colors.white,
               ),
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 30,
-                    ),
-                    SizedBox(
-                        height: 50, width: 50, child: Icon(Icons.arrow_back)),
-                  ],
-                ),
-              ),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        SizedBox(
+                            height: 50,
+                            width: 50,
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.blue,
+                              size: 30,
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 300,
+                            child: Text(
+                              "Fiddle Leaf",
+                              style: TextStyle(
+                                  color: Color(0xff0e1b30),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 32),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '10" Nursery Pot',
+                            style: TextStyle(color: Colors.black45),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: Text('\$',
+                                    style: TextStyle(
+                                        color: Color(0xff343434),
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                              Text('85',
+                                  style: TextStyle(
+                                      color: Color(0xff343434),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 52.0)),
+                            ],
+                          ),
+                        ),
+                      ])),
             ),
           ),
           SizedBox(
             height: 140,
-          )
+          ),
         ],
       ),
     );
